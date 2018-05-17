e322d656c2f9b296145d4e0b61d0284f273a0503
Cobey Hollier <cobey.hollier@mac.com>
2018-05-16 17:26:23 -0700

    Adds the MasterDeck class

--- Freecell2/Model/Game/MasterDeck.swift
+++ Freecell2/Model/Game/MasterDeck.swift
@@ -7,6 +7,63 @@
 //
 
 import Foundation
-class MasterDeck {
+class MasterDeck: ContainsCard, HasState, Resetable {
+    var cards: [Card]
+    var state: State = .empty
     
+    init() {
+        self.cards = []
+    }
+    
+    var bottomCard: Card? {
+        return cards.last
+    }
+    
+    var isEmpty: Bool {
+        return cards.isEmpty
+    }
+    
+    
+    func removeBottom() {
+        let _ = cards.popLast()
+    }
+    
+    
+    func isBottom(card: Card) -> Bool {
+        guard let bottomCard = cards.last else {
+            return false
+        }
+        return card == bottomCard
+    }
+    
+    func removeCard(card: Card) {
+        var i = 0
+        for potientialCard in cards {
+            if potientialCard == card {
+                cards.remove(at: i)
+            }
+        i += 1
+        }
+    }
+    
+    
+    func contains(card: Card) -> Bool {
+        for currentCard in cards {
+            if card == currentCard {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func add(card: Card) throws {
+        cards.append(card)
+        state = .card(card)
+        
+    }
+    
+    
+    func reset(with cards: [Card]) {
+        self.cards = cards
+    }
 }
