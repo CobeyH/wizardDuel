7bfc8342d1e58aa2b690d49e113de3165330e26c
Adrian Ruigrok <adrianr@mac.com>
2018-05-27 12:43:10 -0700

    Add authentication

--- Wizard Duel/AppDelegate.swift
+++ Wizard Duel/AppDelegate.swift
@@ -8,12 +8,25 @@
 
 
 import Cocoa
+import FirebaseAuth
 import FirebaseCore
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        //TODO: Log in the user
+
+        FirebaseApp.configure()
+
+        Auth.auth().signIn(withEmail: "chlobey1@gmail.com", password: "acrug3rok") { (user, error) in
+            if error != nil {
+                print("Error logging in")
+            } else {
+                print("loginn Successful")
+                
+            }
+        }
         
     }
     
